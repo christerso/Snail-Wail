f899a3af83e1cf9c581a30eaeee20a68effd1aa3
Christer S <christer.soderlund@hiq.se>
2011-01-31 20:43:11 +0100

    Removed some files

--- src/engine/android/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/christerso/projects/snailwail/src/engine/android
-# Build directory: /home/christerso/projects/snailwail/src/engine/android/build
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(src)
-SUBDIRS(test)
